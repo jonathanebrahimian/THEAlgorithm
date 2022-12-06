4497758900008e6b20f85dca10e119af56b2b79d
jonathanebrahimian <jebrahimian@smu.edu>
2022-12-06 16:10:16 -0600

    testing branch settings

--- local.py
+++ local.py
@@ -9,7 +9,7 @@ tokens = {
     'usd': '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
     'shib': '0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE',
     'uni': '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
-    'ape': '0x4d224452801aced8b2f0aebe155379bb5d594381'  # issue with APE
+    'ape': '0x4d224452801aced8b2f0aebe155379bb5d594381'  
 }
 # lambda_handler(None,None)
 event = {
